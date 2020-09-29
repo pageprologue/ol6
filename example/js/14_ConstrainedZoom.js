4b9e74254828a0a21253b82ae6276d4260cc78f5
Chloe <page.prologue@gmail.com>
2020-09-29 10:47:14 +0900

    - example 14

---
+++ example/js/14_ConstrainedZoom.js
@@ -0,0 +1,28 @@
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import TileLayer from 'ol/layer/Tile';
+import View from 'ol/View';
+import XYZ from 'ol/source/XYZ';
+
+var key = 'My Key';
+var attributions =
+  '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> ' +
+  '<a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>';
+
+var map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new XYZ({
+        attributions: attributions,
+        url:
+          'https://api.maptiler.com/tiles/satellite/{z}/{x}/{y}.jpg?key=' + key,
+      }),
+    }) ],
+  view: new View({
+    center: [-13553864, 5918250],
+    zoom: 11,
+    minZoom: 9,
+    maxZoom: 13,
+  }),
+});
