1373c2a57acc9f7286118840f280acac7e598759
Chloe <page.prologue@gmail.com>
2020-10-08 20:18:13 +0900

    - example 22

---
+++ example/js/22_CustomTooltips.js
@@ -0,0 +1,27 @@
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import View from 'ol/View';
+
+var map = new Map({
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }) ],
+  target: 'map',
+  view: new View({
+    center: [-8730000, 5930000],
+    rotation: Math.PI / 5,
+    zoom: 8,
+  }),
+});
+
+$('.ol-zoom-in, .ol-zoom-out').tooltip({
+  placement: 'right',
+  container: '#map',
+});
+$('.ol-rotate-reset, .ol-attribution button[title]').tooltip({
+  placement: 'left',
+  container: '#map',
+});
